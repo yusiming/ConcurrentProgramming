4b19bf06ed4f643504ea6ba227eaff90916887fa
虞四明 <13966931523@163.com>
2019-02-28 22:20:41 +0800

    使用synchronized关键字来保证原子性（这里的原子性是针对某个代码块或者方法而言的）

---
+++ src/keywordVolatile/TestVolatile3.java
@@ -0,0 +1,39 @@
+package keywordVolatile;
+
+/**
+ * 使用synchronized关键字来保证原子性
+ *
+ * @Auther yusiming
+ * @Date 2019/2/28 22:18
+ */
+public class TestVolatile3 {
+    private volatile int count = 0;
+
+    private synchronized void increase() {
+        for (int i = 0; i < 10000; i++) {
+            count++;
+        }
+        System.out.println(count);
+    }
+
+    /**
+     * 结果：
+     * 10000
+     * 20000
+     * 30000
+     * 40000
+     * 50000
+     * 60000
+     * 70000
+     * 80000
+     * 90000
+     * 100000
+     */
+    public static void main(String[] args) {
+        TestVolatile3 t = new TestVolatile3();
+        for (int i = 0; i < 10; i++) {
+            new Thread(t::increase).start();
+        }
+    }
+
+}
