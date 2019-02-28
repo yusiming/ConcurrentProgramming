7f4f532887eda1805517af2f6e04ae312a0f5ddf
虞四明 <13966931523@163.com>
2019-02-28 20:15:13 +0800

    sleep()方法不会释放锁

---
+++ src/keywordSynchronized/SleepWillNotReleaseTheLock.java
@@ -0,0 +1,35 @@
+package keywordSynchronized;
+
+/**
+ * sleep()方法不会释放锁，所以要小心了，
+ * 如果某个线程带着锁永久的沉睡了，那么其他的线程永远都得不到执行机会
+ *
+ * @Auther yusiming
+ * @Date 2019/2/28 20:09
+ */
+public class SleepWillNotReleaseTheLock {
+    public synchronized void fun1() {
+        System.out.println(Thread.currentThread().getName() + " 我进来了！");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread().getName() + " 我出去了");
+    }
+
+    /**
+     * 执行结果：
+     * Thread-0 我进来了！
+     * Thread-0 我出去了
+     * Thread-1 我进来了！
+     * Thread-1 我出去了
+     * <p>
+     * 在Thread-0 sleep的5中秒中内，Thread-1无法进入方法
+     */
+    public static void main(String[] args) {
+        SleepWillNotReleaseTheLock s = new SleepWillNotReleaseTheLock();
+        new Thread(s::fun1).start();
+        new Thread(s::fun1).start();
+    }
+}
