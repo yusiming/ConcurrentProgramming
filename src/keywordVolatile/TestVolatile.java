cbee173949536806646f12005c51440d791d8343
虞四明 <13966931523@163.com>
2019-02-28 22:03:10 +0800

    volatile关键字能够保证可见性

---
+++ src/keywordVolatile/TestVolatile.java
@@ -0,0 +1,31 @@
+package keywordVolatile;
+
+/**
+ * volatile关键字能够保证可见性：
+ * 即一个线程修改了一个被volatile修饰的变量的值，其他线程都可以立即知道
+ *
+ * @Auther yusiming
+ * @Date 2019/2/28 21:37
+ */
+public class TestVolatile {
+    private /*volatile*/ boolean running = true;
+    // 也可以修饰引用类型
+    // private volatile Object object = new Object();
+
+    public void fun() {
+        while (running) {
+            //
+        }
+    }
+
+    public static void main(String[] args) {
+        TestVolatile t = new TestVolatile();
+        new Thread(t::fun).start();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        t.running = false;
+    }
+}
