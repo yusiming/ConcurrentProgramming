c0465037582d5755142e998a7d7583dc0080a304
虞四明 <13966931523@163.com>
2019-02-28 22:24:24 +0800

    使用原子类的各种方法保证原子性

---
+++ src/keywordVolatile/TestVolatile4.java
@@ -0,0 +1,43 @@
+package keywordVolatile;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 使用原子类，即一系列的AtomXXX类也可以保证原子性
+ * 而且一般情况下效率会更高
+ *
+ * @Auther yusiming
+ * @Date 2019/2/28 22:21
+ */
+public class TestVolatile4 {
+    private AtomicInteger count = new AtomicInteger(0);
+
+    private synchronized void increase() {
+        for (int i = 0; i < 10000; i++) {
+            // 原子性操作
+            count.incrementAndGet();
+        }
+        System.out.println(count.get());
+    }
+
+    /**
+     * 结果：
+     * 10000
+     * 20000
+     * 30000
+     * 40000
+     * 50000
+     * 60000
+     * 70000
+     * 80000
+     * 90000
+     * 100000
+     */
+    public static void main(String[] args) {
+        TestVolatile4 t = new TestVolatile4();
+        for (int i = 0; i < 10; i++) {
+            new Thread(t::increase).start();
+        }
+    }
+
+}
