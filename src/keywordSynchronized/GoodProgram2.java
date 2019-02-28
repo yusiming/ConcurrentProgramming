c6a0f7cfb905957c0c8638e7f9b10a6caf2a47f7
虞四明 <13966931523@163.com>
2019-02-28 19:59:10 +0800

    synchronized关键字的使用方式2

---
+++ src/keywordSynchronized/GoodProgram2.java
@@ -0,0 +1,19 @@
+package keywordSynchronized;
+
+/**
+ * @Auther yusiming
+ * @Date 2019/2/28 19:51
+ */
+public class GoodProgram2 {
+    private int count = 10;
+
+    private void increase() {
+        // 任何对象要执行下面的代码，就必须先拿到this，即当前对象的锁
+        // 如果锁已经被某个线程持有了，那么当前的线程就必须等待
+        // 直到持有锁的线程释放锁，才有机会执行
+        synchronized (this) {
+            count--;
+            System.out.println(Thread.currentThread().getName() + ": " + count);
+        }
+    }
+}
