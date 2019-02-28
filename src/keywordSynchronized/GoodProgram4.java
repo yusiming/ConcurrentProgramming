35080817c9c11e9f9dbeb73f0729b1048ffb1ae5
虞四明 <13966931523@163.com>
2019-02-28 19:59:25 +0800

    synchronized关键字的使用方式4

---
+++ src/keywordSynchronized/GoodProgram4.java
@@ -0,0 +1,26 @@
+package keywordSynchronized;
+
+/**
+ * @Auther yusiming
+ * @Date 2019/2/28 19:56
+ */
+public class GoodProgram4 {
+    private static int count = 10;
+
+    /**
+     * 如果将synchronized关键字直接加到静态方法的声明中
+     * 就等同于方法执行时：synchronized(GoodProgram.class)
+     */
+    private static synchronized void increase1() {
+        count--;
+        System.out.println(Thread.currentThread().getName() + ": " + count);
+    }
+
+    private static synchronized void increase2() {
+        // 这里肯定是不能对 this 加锁的
+        synchronized (GoodProgram4.class) {
+            count--;
+            System.out.println(Thread.currentThread().getName() + ": " + count);
+        }
+    }
+}
