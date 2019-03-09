b65a04bdb26f0b4bebcef8c2d4d01acb9c9cd488
虞四明 <13966931523@163.com>
2019-03-09 15:11:43 +0800

    使用锁或者原子变量保证原子性和内存可见性

--- src/javaConcurrencyInParctice/case2/UseSynchronizerMakeAtomicity.java
+++ src/javaConcurrencyInParctice/case2/UseSynchronizerMakeAtomicity.java
@@ -1,16 +1,16 @@
 package javaConcurrencyInParctice.case2;
 
 /**
- * 使用volatile修饰的变量，并不能保证对于该变量的操作是原子性的
- * 所以当需要保证对某个变量的操作是原子性的时候，必须使用锁或者原子变量来进行同步
+ * 使用锁或者原子变量保证原子性和内存可见性
  *
  * @author yusiming
- * @date 2019/3/9 11:39
+ * @date 2019/3/9 15:06
  */
-public class UseVolatileCannotMakeAtomicity {
+public class UseSynchronizerMakeAtomicity {
     private static int count = 0;
+    // private static AtomicInteger count = new AtomicInteger(0);
 
-    public static void add() {
+    public /*synchronized*/ static void add() {
         for (int i = 0; i < 100000; i++) {
             count++;
         }
@@ -18,7 +18,7 @@ public class UseVolatileCannotMakeAtomicity {
 
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-            new Thread(UseVolatileCannotMakeAtomicity::add).start();
+            new Thread(UseSynchronizerMakeAtomicity::add).start();
         }
         while (Thread.activeCount() > 2) {
             Thread.yield();
@@ -26,4 +26,5 @@ public class UseVolatileCannotMakeAtomicity {
         // 总是一个小于100000的值
         System.out.println(count);
     }
+
 }
